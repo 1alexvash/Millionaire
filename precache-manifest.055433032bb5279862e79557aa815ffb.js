624186fd3fa2bebc6cc9e652589bc1d4ec0d8721
Alexander <1alexvash@gmail.com>
2020-02-08 21:13:51 +0200

    Updates

---
+++ precache-manifest.055433032bb5279862e79557aa815ffb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe3b05d6526d4109255dd5086e3a3a3",
+    "url": "/Millionaire/index.html"
+  },
+  {
+    "revision": "5e5496ec07363a59a818",
+    "url": "/Millionaire/static/css/main.64aecf84.chunk.css"
+  },
+  {
+    "revision": "7146a5ef4ec9ed349a5b",
+    "url": "/Millionaire/static/js/2.7ca04fa4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Millionaire/static/js/2.7ca04fa4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e5496ec07363a59a818",
+    "url": "/Millionaire/static/js/main.6ad35065.chunk.js"
+  },
+  {
+    "revision": "13712810f8fed92d43c4",
+    "url": "/Millionaire/static/js/runtime-main.6ed37a87.js"
+  },
+  {
+    "revision": "4114f0acb01a9cc2ef72b9187e7b8c1e",
+    "url": "/Millionaire/static/media/money-bg.4114f0ac.jpg"
+  },
+  {
+    "revision": "545f63e3d1003e629450f6017d83582d",
+    "url": "/Millionaire/static/media/stonks.545f63e3.jpg"
+  }
+]);
